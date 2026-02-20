c20edb7562349bc2e3c251083f587503c4418ed4
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-20 22:32:53 +0530

    added

--- src/MultipleCatch.java
+++ src/MultipleCatch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultipleCatch {   // ← class name same as file name
+public class MultipleCatch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
